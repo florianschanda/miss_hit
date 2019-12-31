8618e22a1db30c837c9420f61e8830016c797068
Florian Schanda <florian@schanda.org.uk>
2019-12-31 17:03:59 +0100

    Add another case to test for #55

--- tests/parser/class_keywords/potato.m
+++ tests/parser/class_keywords/potato.m
@@ -7,9 +7,15 @@ classdef potato
   end
 
   methods
+    % allowed
     function r = foo(self)
       properties = x;
       r = properties;
+    end
+    
+    % allowed but idiotic
+    function p = properties(self)
+        p = 123;
     end
   end
 end
