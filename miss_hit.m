b636520d31760e0e39326524dfd84c57c6ad816f
Benedikt Schmid <benedikt.schmid@zenuity.com>
2020-01-21 16:14:10 +0100

    Add methods to run miss_hit on cached files and commits

--- miss_hit.m
+++ miss_hit.m
@@ -24,6 +24,22 @@ classdef miss_hit < handle
 
         end
 
+        function cached()
+            % Run miss_hit on cached files in repository.
+
+            files = git_retrieve_files('git --no-pager diff --name-only');
+            miss_hit.run(files);
+
+        end
+
+        function lastCommit()
+            % Run miss_hit on files changed in the last commit.
+
+            files = git_retrieve_files('git --no-pager diff HEAD^ HEAD --name-only');
+            miss_hit.run(files);
+
+        end
+
         function file(file)
             % Run miss_hit on given file.
 
@@ -51,21 +67,27 @@ classdef miss_hit < handle
 
         function run(file_or_folder)
 
-            assert(ischar(file_or_folder) && ...
-                (isfolder(file_or_folder) || isfile(file_or_folder)), ...
-                'Input has to be a file or folder.');
+            AssertFileOrFolder = @(f) assert(ischar(f) && (isfolder(f) || isfile(f)), ...
+                'Not a file or folder.');
+
+            if iscell(file_or_folder)
+                cellfun(AssertFileOrFolder, file_or_folder);
+                file_or_folder = strjoin(file_or_folder);
+            else
+                AssertFileOrFolder(file_or_folder);
+            end
 
             % Do the auto-fixes
             python_command = strjoin({ ...
                 'cd', miss_hit.here, '&&', ...
-                'python', 'mh_style.py', file_or_folder, '--fix', '--no-style'});
+                'python3', 'mh_style.py', file_or_folder, '--fix', '--no-style'});
             system(python_command, '-echo');
 
             % Generate report for remaining issues.
             report_name = fullfile(miss_hit.here, 'report.html');
             python_command = strjoin({ ...
                 'cd', miss_hit.here, '&&', ...
-                'python', 'mh_style.py', file_or_folder, ...
+                'python3', 'mh_style.py', file_or_folder, ...
                     '--html', report_name});
             system(python_command);
 
@@ -74,5 +96,19 @@ classdef miss_hit < handle
         end
 
     end
+
+end
+
+function files = git_retrieve_files(msg)
+
+    fldr = cd();
+    [s, m] = unix(strjoin({'cd', fldr, '&&', msg}));
+    assert(s == 0, 'Could not execute git command.');
+
+    f = strsplit(m);
+    f(cellfun(@isempty, f)) = [];
+    assert(~isempty(f), 'No files changed.');
+
+    files = cellfun(@(e) fullfile(fldr, e), f, 'un', false)';
 
 end
