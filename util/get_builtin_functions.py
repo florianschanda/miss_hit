af24d2cc418700b3e50020a8889ae4aa007b0bfe
Florian Schanda <florian@schanda.org.uk>
2020-02-13 07:54:47 +0100

    More precise script to extract builtin names
    * Pull out classes and namespaces
    * Distinguish between extra functions (implemented in the toolbox)
      and true built-in functions
    * Special case Content

--- util/get_builtin_functions.py
+++ util/get_builtin_functions.py
@@ -33,25 +33,85 @@ def main():
 
     toolbox = os.path.join(options.matlab_root, "toolbox", "matlab")
 
-    functions = set()
+    builtin_functions = set()
+    extra_functions = set()
+    namespaces = set()
+    builtin_classes = set()
+    special_vars = set(["Contents"])
 
     for path, dirs, files in os.walk(toolbox):
         dirs.sort()
+
+        # Exclude packages (they are already in a different namespace)
+        for i in reversed(range(len(dirs))):
+            if dirs[i].startswith("+"):
+                namespaces.add(dirs[i][1:])
+                del dirs[i]
+            elif dirs[i].startswith("@"):
+                builtin_classes.add(dirs[i][1:])
+                del dirs[i]
+            elif dirs[i] == "demos":
+                # Remove examples
+                del dirs[i]
+
         for f in files:
-            if f.endswith(".m"):
+            if f.endswith(".m") and f not in ("Contents.m", "debug.m"):
                 with open(os.path.join(path, f), "r") as fd:
                     tmp = fd.read()
+                name = f[:-2]
                 if re.search("[Bb]uilt-?in function", tmp):
-                    name = f[:-2]
-                    functions.add(name)
+                    builtin_functions.add(name)
+                else:
+                    classified = False
+                    for line in tmp.splitlines():
+                        if line.strip().startswith("function"):
+                            extra_functions.add(name)
+                            classified = True
+                            break
+                        elif line.strip().startswith("classdef"):
+                            builtin_classes.add(name)
+                            classified = True
+                            break
+                    if not classified:
+                        # We assume it's just a builtin function at this point
+                        builtin_functions.add(name)
 
-    functions -= KEYWORDS
+    # Some of the basic statements have help text associated. We
+    # remove them at this point.
+    builtin_functions -= KEYWORDS
+
+    # For some reason the classdef construct is not documented as a
+    # built-in. We remove that one specifically here, and otherwise
+    # assert that a non-builtin function will have code attached.
+    extra_functions -= set(["classdef"])
+    assert len(extra_functions & KEYWORDS) == 0
 
     with open(os.path.join("..", "m_language_builtins.py"), "w") as fd:
         fd.write("# This file is auto-generated with util/get_builtin_functions\n")
         fd.write("# for MATLAB release %s\n\n" % version)
+
         fd.write("BUILTIN_FUNCTIONS = frozenset([\n")
-        for fn in sorted(functions):
+        for fn in sorted(builtin_functions):
+            fd.write("    \"%s\",\n" % fn)
+        fd.write("])\n\n")
+
+        fd.write("EXTRA_FUNCTIONS = frozenset([\n")
+        for fn in sorted(extra_functions):
+            fd.write("    \"%s\",\n" % fn)
+        fd.write("])\n\n")
+
+        fd.write("BUILTIN_CLASSES = frozenset([\n")
+        for fn in sorted(builtin_classes):
+            fd.write("    \"%s\",\n" % fn)
+        fd.write("])\n\n")
+
+        fd.write("BUILTIN_NAMESPACES = frozenset([\n")
+        for fn in sorted(namespaces):
+            fd.write("    \"%s\",\n" % fn)
+        fd.write("])\n\n")
+
+        fd.write("SPECIAL_NAMES = frozenset([\n")
+        for fn in sorted(special_vars):
             fd.write("    \"%s\",\n" % fn)
         fd.write("])\n")
 
