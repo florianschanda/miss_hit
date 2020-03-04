0451cf630c823fc03ea4950096bf021c8b90950c
Florian Schanda <florian@schanda.org.uk>
2020-03-04 07:36:08 +0100

    #119 Make sure pragmas in class files work
    Closes #119

---
+++ tests/metrics/justifications/class_file.m
@@ -0,0 +1,13 @@
+% (c) Copyright 2020 Florian Schanda
+
+%| pragma Justify(metric, "file_length", "seems OK");
+
+classdef class_file
+
+    properties
+        x
+    end
+
+end
+
+%| pragma Justify(metric, "npath", "(does not apply)");
