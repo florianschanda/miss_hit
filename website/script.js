51584905b6149fa37d9ec079aaf70a2585fc5230
Florian Schanda <florian@schanda.org.uk>
2021-05-16 18:53:33 +0200

    #42 Partially integrate new website style

---
+++ website/script.js
@@ -0,0 +1,10 @@
+
+showMenu = function () {
+    document.body.style.overflow = "hidden";
+    document.body.style.height = "100%";
+}
+
+hideMenu = function () {
+    document.body.style.overflow = "auto";
+    document.body.style.height = "auto";
+}
