05b1f824c0d3ae9202602479da97c5030d6ff964
Florian Schanda <florian@schanda.org.uk>
2023-05-16 15:39:58 +0200

    #265 Show that the desired output can already be reached
    Closing #265

--- tests/style/issue_265_alternative_indent/test_from_ticket.m
+++ tests/style/issue_265_alternative_indent/test_from_ticket.m
@@ -11,14 +11,14 @@ statement2;
 %% -------- local functions -------
 
 % comment for relatively short localfun 1
-function output=localfun1(input)
+function output = localfun1(input)
     % local-functions, or functions with an "end" at the bottom should be indented
     statement1
     statement2
 end
 
 % comment for relatively short localfun 2
-function output=localfun2(input)
+function output = localfun2(input)
     % short comment on
     statement1
     statement2
