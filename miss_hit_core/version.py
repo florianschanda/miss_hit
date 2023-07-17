be321839aa1fae575a815f5aad93d45b4b993ef8
Florian Schanda <florian@schanda.org.uk>
2023-07-17 10:47:19 +0200

    Bump version to 0.9.43-dev after release

--- miss_hit_core/version.py
+++ miss_hit_core/version.py
@@ -27,8 +27,8 @@
 
 GITHUB_ISSUES = "https://github.com/florianschanda/miss_hit/issues"
 
-VERSION_TUPLE = (0, 9, 42)
-VERSION_SUFFIX = ""
+VERSION_TUPLE = (0, 9, 43)
+VERSION_SUFFIX = "dev"
 
 VERSION = ("%u.%u.%u" % VERSION_TUPLE) + \
           ("-%s" % VERSION_SUFFIX if VERSION_SUFFIX else "")
